c9dfcafb8bdc4f5fc1a70f9bc5b1fa85aac4fdb4
Davi Sopher <dssopher@gmail.com>
2023-08-27 15:01:40 +0300

    First commit: person, Patient and Doctor

---
+++ Hospital OOP/indexH.ts
@@ -0,0 +1,38 @@
+// class Person {
+//     firstName: string;
+//     lastName: string;
+
+
+//     constructor(firstName: string, lastName: string){
+//       this.firstName = firstName
+//       this.lastName = lastName
+//     }
+
+// }
+// class Patient extends Person {
+//     patientId: string;
+
+//     constructor(firstName: string, lastName: string, patientId: string) {
+//         super(firstName,lastName)
+//         this.patientId = patientId;
+//     }
+// }
+
+// let pet = new Patient('David','Sopher','1')
+// console.log(pet)
+
+// class Doctor extends Person {
+//     doctorID: string;
+//     specialization: string;
+
+//     constructor(firstName: string, lastName: string, doctorId: string, specialization: string) {
+//         super(firstName,lastName);
+//         this.doctorID = doctorId;
+//         this.specialization = specialization
+//     }
+// }
+// let doc = new Doctor('David','Sopher','1', 'oralogy')
+// console.log(doc)
+
+
+
